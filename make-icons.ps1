35a17d4b2e5845ddc928f95f6db91ffd1f71f50c
Tuo Nome <tua-email@example.com>
2026-06-04 09:59:06 +0200

    feat: icona app elefante Venice AI su sfondo verde

--- make-icons.ps1
+++ make-icons.ps1
@@ -3,16 +3,25 @@ Add-Type -AssemblyName System.Drawing
 function New-Icon($size, $outPath) {
   $bmp = New-Object System.Drawing.Bitmap($size, $size)
   $g = [System.Drawing.Graphics]::FromImage($bmp)
-  $g.Clear([System.Drawing.Color]::FromArgb(255, 217, 119, 6))
-  $fontSize = [int]($size * 0.35)
-  $font = New-Object System.Drawing.Font("Arial", $fontSize, [System.Drawing.FontStyle]::Bold)
-  $brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White)
-  $sf = New-Object System.Drawing.StringFormat
-  $sf.Alignment = [System.Drawing.StringAlignment]::Center
-  $sf.LineAlignment = [System.Drawing.StringAlignment]::Center
-  $rect = New-Object System.Drawing.RectangleF(0, 0, $size, $size)
-  $g.DrawString("CT", $font, $brush, $rect, $sf)
+
+  # Sfondo verde #6DAB3C
+  $g.Clear([System.Drawing.Color]::FromArgb(255, 109, 171, 60))
+
+  # Carica logo elefante e rende bianco trasparente
+  $src = [System.Drawing.Bitmap]::FromFile("$PSScriptRoot\public\VeniceAI_3tJwOMd.png")
+  $src.MakeTransparent([System.Drawing.Color]::White)
+
+  # Padding negativo: zooma sull'elefante ritagliando lo spazio bianco del PNG sorgente
+  $pad = [int]($size * -0.20)
+  $inner = $size - ($pad * 2)
+  $destRect = New-Object System.Drawing.Rectangle($pad, $pad, $inner, $inner)
+
+  $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+  $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
+  $g.DrawImage($src, $destRect)
+
   $g.Dispose()
+  $src.Dispose()
   $bmp.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
   $bmp.Dispose()
 }
